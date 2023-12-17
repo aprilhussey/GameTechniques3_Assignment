ea7d38af6e03a889fd827cf3fbea88411f7672e5
April Hussey <54184656+aprilhussey@users.noreply.github.com>
2023-12-17 22:54:09 +0000

    Fixed animations
    Todo:
    * Re-implement shooting mechanic

--- Kitty Carnage/Assets/Scripts/PlayerAnimationController.cs
+++ Kitty Carnage/Assets/Scripts/PlayerAnimationController.cs
@@ -8,20 +8,21 @@ public class PlayerAnimationController : MonoBehaviour
 	private PlayerController playerController;
 
 	private Animator animator;
-    private Rigidbody rb;
+    private Rigidbody playerRigidbody;
 
 	const float locomotionAnimationSmoothTime = 0.1f;
 
 	private Vector2 currentAnimationBlend = new Vector2();
 	private Vector2 animationVelocity = new Vector2();
-	[SerializeField] private float animationSmoothTime = 0.1f;
+	[SerializeField]
+	private float animationSmoothTime = 0.1f;
 
 	// Awake is called before Start
 	void Awake()
     {
         playerController = GetComponent<PlayerController>();
         animator = GetComponent<Animator>();
-		rb = GetComponent<Rigidbody>();
+		playerRigidbody = GetComponent<Rigidbody>();
 	}
     
     // Start is called before the first frame update
@@ -33,29 +34,29 @@ public class PlayerAnimationController : MonoBehaviour
     void Update()
     {
 		// Calculate speed of player
-        float currentSpeed = rb.velocity.magnitude;
+        float currentSpeed = playerRigidbody.velocity.magnitude;
 
 		// Normalize the speed to a value between 0 and 1
 		float normalizedSpeed = currentSpeed / playerController.speed;
 
 		// Set the "speed" parameter in the animator
-		animator.SetFloat("Speed", normalizedSpeed, locomotionAnimationSmoothTime, Time.deltaTime);
+		animator.SetFloat("speed", normalizedSpeed, locomotionAnimationSmoothTime, Time.deltaTime);
 
 		// Blend animations
 		currentAnimationBlend = Vector2.SmoothDamp(currentAnimationBlend, playerController.movementInput,
 			ref animationVelocity, animationSmoothTime);
 
-		animator.SetFloat("Horizontal Input", currentAnimationBlend.x);
-		animator.SetFloat("Vertical Input", currentAnimationBlend.y);
+		animator.SetFloat("horizontalInput", currentAnimationBlend.x);
+		animator.SetFloat("verticalInput", currentAnimationBlend.y);
 
 		// Walking
 		if (currentAnimationBlend.x != 0 || currentAnimationBlend.y != 0)
 		{
-			animator.SetBool("Walking", true);
+			animator.SetBool("walking", true);
 		}
 		else if (currentAnimationBlend.x == 0 || currentAnimationBlend.y == 0)
 		{
-			animator.SetBool("Walking", false);
+			animator.SetBool("walking", false);
 		}
 	}
 }
