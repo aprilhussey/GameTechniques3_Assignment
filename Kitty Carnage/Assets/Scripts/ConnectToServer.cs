9930cca2c190d2427826745d13f360a3c70ae996
April Hussey <54184656+aprilhussey@users.noreply.github.com>
2024-01-11 01:15:30 +0000

    Implemented basic lobby scene

--- Kitty Carnage/Assets/Scripts/ConnectToServer.cs
+++ Kitty Carnage/Assets/Scripts/ConnectToServer.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviour
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
-    }
+        PhotonNetwork.ConnectUsingSettings();   // Connect to Photon server
+	}
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();  // Join lobby
+	}
+
+	public override void OnJoinedLobby()
+	{
+        SceneManager.LoadScene("Lobby");    // Load Lobby scene
+	}
 }
