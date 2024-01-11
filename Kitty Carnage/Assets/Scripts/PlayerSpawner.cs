5b40afe9385a674fd5a5cf0f9542b0fa6ec345b3
April Hussey <54184656+aprilhussey@users.noreply.github.com>
2024-01-11 04:20:43 +0000

    Implemented player spawner

---
+++ Kitty Carnage/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    public List<Transform> playerSpawnLocations = new List<Transform>();
+
+    void Start()
+    {
+		// Generate a random index
+		int randomIndex = Random.Range(0, playerSpawnLocations.Count);
+
+		// Get the spawn location at the randome index
+		Transform spawnLocation = playerSpawnLocations[randomIndex];
+
+		// Instantiate the player at the spawn location
+		PhotonNetwork.Instantiate(playerPrefab.name, spawnLocation.position, spawnLocation.rotation);
+	}
+}
